fdeeed9d55ed93a50abce46aa3e6ca8aa1b8a5c8
AndriantoAGun <andrianto22491@gmail.com>
2019-07-18 08:14:01 +0700

    Tugas 8 JavaScript
    tugas Niomic tentang JavaScript meliputi data Object

---
+++ tugas8.js
@@ -0,0 +1,9 @@
+var Summon = { Level : 200, Health : 1128, Attack : 2850, Cooldown : 9 };
+
+console.log(Summon.Level);
+console.log(Summon.Health);
+console.log(Summon.Attack);
+console.log(Summon.Cooldown);
+
+Summon.Cooldown = 7;
+console.log(Summon.Cooldown);
